1133e2aaf1f3b6b516ab5af967202ed2d7a3393f
firaterdm <33firaterdem@gmail.com>
2024-07-19 16:20:00 +0300

    Update NativeTokenSymbol.tsx

--- src/components/NativeToken/NativeTokenSymbol.tsx
+++ src/components/NativeToken/NativeTokenSymbol.tsx
@@ -2,7 +2,7 @@ import { SVGProps } from 'react';
 import { useSelector } from 'react-redux';
 
 import { ReactComponent as EgldSymbol } from 'assets/img/tokens/egld-symbol.svg';
-import { ReactComponent as SpcLogo } from 'assets/img/tokens/spc-logo.svg';
+import { ReactComponent as CnetLogo } from 'assets/img/tokens/cnet-logo.svg';
 import { isEgldToken } from 'helpers';
 import { activeNetworkSelector } from 'redux/selectors';
 
@@ -15,8 +15,8 @@ export const NativeTokenSymbol = (props: SVGProps<SVGSVGElement>) => {
   }
 
   switch (egldLabel?.toLowerCase()) {
-    case 'spc':
-      return <SpcLogo {...props} />;
+    case 'xcnet':
+      return <CnetLogo {...props} />;
     default:
       return null;
   }
